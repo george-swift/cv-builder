7cbff6f4b24f3c9d00c942b1823918298bdc121c
George <71434803+george-swift@users.noreply.github.com>
2021-06-21 02:19:50 +0100

    Add header and build components to app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from './components/Header';
+import BuildSection from './components/Builder';
+
+const App = () => (
+  <>
+    <Header />
+    <BuildSection />
+  </>
+);
+
+export default App;
