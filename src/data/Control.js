e7dd30c6cd975e18bc2677afed0620977c08efad
George <71434803+george-swift@users.noreply.github.com>
2021-06-21 02:17:12 +0100

    Add values for state

---
+++ src/data/Control.js
@@ -0,0 +1,36 @@
+import { v4 } from 'uuid';
+
+const controlledFields = {
+  personalInfo: {
+    firstName: '',
+    lastName: '',
+    professionalTitle: '',
+    emailAddress: '',
+    phoneNumber: '',
+    website: '',
+  },
+
+  experience: [
+    {
+      id: v4(),
+      position: '',
+      company: '',
+      city: '',
+      from: '',
+      to: '',
+    },
+  ],
+
+  education: [
+    {
+      id: v4(),
+      universityName: '',
+      major: '',
+      city: '',
+      from: '',
+      to: '',
+    },
+  ],
+};
+
+export default controlledFields;
