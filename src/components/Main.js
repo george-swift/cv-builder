cc1a6523785aac7a560a2cede1c2c871b4b7ca9f
George <71434803+george-swift@users.noreply.github.com>
2021-06-23 22:23:36 +0100

    Pass reset action to form button

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,45 +2,41 @@ import React, { useReducer } from 'react';
 import CvForm from './form/FormLayout';
 import initialState from '../data/static';
 import reducer from './reducer';
-import main from '../styles/main.module.css';
 
 const FormManager = () => {
   const [state, dispatch] = useReducer(reducer, initialState);
 
-  const editPersonalInfo = (e) => {
-    dispatch({
-      type: 'EDIT_INFO',
-      payload: e.target,
-    });
-  };
+  const editPersonalInfo = (e) => dispatch({
+    type: 'EDIT_INFO',
+    payload: e.target,
+  });
 
-  const editExperience = (e, id) => {
-    dispatch({
-      type: 'EDIT_EXPERIENCE',
-      payload: e.target,
-      id,
-    });
-  };
+  const editExperience = (e, id) => dispatch({
+    type: 'EDIT_EXPERIENCE',
+    payload: e.target,
+    id,
+  });
 
-  const editEducation = (e, id) => {
-    dispatch({
-      type: 'EDIT_EDUCATION',
-      payload: e.target,
-      id,
-    });
-  };
+  const editEducation = (e, id) => dispatch({
+    type: 'EDIT_EDUCATION',
+    payload: e.target,
+    id,
+  });
 
   const addExperience = () => dispatch({ type: 'ADD_EXPERIENCE_SECTION' });
-
   const deleteExperience = (id) => dispatch({ type: 'DELETE_EXPERIENCE_SECTION', id });
 
   const addEducation = () => dispatch({ type: 'ADD_EDUCATION_SECTION' });
-
   const deleteEducation = (id) => dispatch({ type: 'DELETE_EDUCATION_SECTION', id });
+
+  const resetFields = () => dispatch({
+    type: 'RESET_FIELDS',
+    reset: initialState,
+  });
 
   return (
     <main className="container p-4">
-      <div className={main.formWrapper}>
+      <div className="formWrapper">
         <CvForm
           cv={state}
           onChangePersonal={editPersonalInfo}
@@ -50,6 +46,7 @@ const FormManager = () => {
           onChangeEducation={editEducation}
           onAddEducation={addEducation}
           onDeleteEducation={deleteEducation}
+          onReset={resetFields}
         />
       </div>
     </main>
