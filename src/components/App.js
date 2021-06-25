88aed98c8cc1b8d6b9a176406277610e215ade12
George <71434803+george-swift@users.noreply.github.com>
2021-06-25 14:54:42 +0100

    Add links and routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,52 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+} from 'react-router-dom';
 import Header from './Header';
 import FormManager from './Main';
 import '../styles/App.css';
+import About from './About';
+import Preview from './Preview';
 
 const App = () => (
-  <>
+  <Router>
     <Header />
-    <FormManager />
-  </>
+
+    <main className="container-fluid">
+      <div className="row py-4">
+        <div className="col-md-3 d-flex flex-column mt-4">
+          <Link
+            to="/about"
+            className="btn btn-outline-teal mb-3"
+          >
+            About
+          </Link>
+          <Link
+            to="/editor"
+            className="btn btn-outline-teal mb-3"
+          >
+            Editor
+          </Link>
+          <Link
+            to="/preview"
+            className="btn btn-outline-teal mb-3"
+          >
+            Preview
+          </Link>
+        </div>
+        <div className="col-md-9 mt-4 wrapper">
+          <Routes>
+            <Route path="/about" element={<About />} />
+            <Route path="/editor" element={<FormManager />} />
+            <Route path="/preview" element={<Preview />} />
+          </Routes>
+        </div>
+      </div>
+    </main>
+  </Router>
 );
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { FaFileAlt } from 'react-icons/fa';
 
 const Header = () => (
-  <header className="header">
-    <span className="me-2">
-      <FaFileAlt />
-    </span>
-    <h1>CV Builder</h1>
+  <header className="header sticky-top">
+    <nav className="navbar">
+      <div className="container-fluid">
+        <Link
+          to="/"
+          className="navbar-brand logo text-white"
+        >
+          <FaFileAlt />
+          <span className="ms-2">CV Builder</span>
+        </Link>
+      </div>
+    </nav>
   </header>
 );
 
