9605a47c7a6bac24f2a53721f99dd650519cc4ad
George <71434803+george-swift@users.noreply.github.com>
2021-06-30 00:36:37 +0100

    Initialise state in App and pass props to children

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import {
   BrowserRouter as Router,
   Routes,
@@ -10,43 +10,49 @@ import FormManager from './Main';
 import '../styles/App.css';
 import About from './About';
 import Preview from './Preview';
+import initialState from '../data/static';
+import reducer from './reducer';
 
-const App = () => (
-  <Router>
-    <Header />
+const App = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
 
-    <main className="container-fluid">
-      <div className="row py-4">
-        <div className="col-md-3 d-flex flex-column mt-4">
-          <Link
-            to="/about"
-            className="btn btn-outline-teal mb-3"
-          >
-            About
-          </Link>
-          <Link
-            to="/editor"
-            className="btn btn-outline-teal mb-3"
-          >
-            Editor
-          </Link>
-          <Link
-            to="/preview"
-            className="btn btn-outline-teal mb-3"
-          >
-            Preview
-          </Link>
+  return (
+    <Router>
+      <Header />
+
+      <main className="container-fluid">
+        <div className="row py-4">
+          <div className="col-md-3 d-flex flex-column mt-4">
+            <Link
+              to="/about"
+              className="btn btn-outline-teal mb-3"
+            >
+              About
+            </Link>
+            <Link
+              to="/editor"
+              className="btn btn-outline-teal mb-3"
+            >
+              Editor
+            </Link>
+            <Link
+              to="/preview"
+              className="btn btn-outline-teal mb-3"
+            >
+              Preview
+            </Link>
+          </div>
+          <div className="col-md-9 mt-4 wrapper">
+            <Routes>
+              <Route path="/about" element={<About />} />
+              <Route path="/editor" element={<FormManager state={state} dispatch={dispatch} />} />
+              <Route path="/preview" element={<Preview data={state} />} />
+            </Routes>
+          </div>
         </div>
-        <div className="col-md-9 mt-4 wrapper">
-          <Routes>
-            <Route path="/about" element={<About />} />
-            <Route path="/editor" element={<FormManager />} />
-            <Route path="/preview" element={<Preview />} />
-          </Routes>
-        </div>
-      </div>
-    </main>
-  </Router>
-);
+      </main>
+    </Router>
+  );
+};
 
 export default App;
